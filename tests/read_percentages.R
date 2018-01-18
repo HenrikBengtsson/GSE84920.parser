e44d04f16691824a7d685b4cbb4df518a6279497
hb <hb@aroma-project.org>
2018-01-17 19:14:18 -0800

    Tweak file name to match the original one

--- tests/read_percentages.R
+++ tests/read_percentages.R
@@ -1,6 +1,6 @@
 library("ramani")
 
 path <- system.file("extdata", package = "ramani", mustWork = TRUE)
-file <- file.path(path, "GSM2254215_ML1_rows=1-50.percentages.txt.gz")
+file <- file.path(path, "GSM2254215_ML1.rows=1-50.percentages.txt.gz")
 data <- read_percentages(file)
 print(data)
